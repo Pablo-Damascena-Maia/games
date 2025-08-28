02cc000d25a1b6f84c8849a59b9d5d27b6ae1bb8
Pablo <pablomaia533@gmail.com>
2025-08-28 11:56:28 -0300

    Delete games directory

--- games/src/main/java/com/senac/games/repository/InscricaoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.senac.games.repository;
-
-import com.senac.games.entity.Categoria;
-import com.senac.games.entity.Inscricao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InscricaoRepository extends JpaRepository<Inscricao,Integer> {
-}
--- games/src/main/java/com/senac/games/repository/PatrocinadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senac.games.repository;
-
-import com.senac.games.entity.Patrocinador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatrocinadorRepository extends JpaRepository<Patrocinador,Integer> {
-}
