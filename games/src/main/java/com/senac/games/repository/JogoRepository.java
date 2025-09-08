c8467c6d42b60695619c6699469c720eb0d01e2a
Pablo <pablomaia533@gmail.com>
2025-09-08 10:22:37 -0300

    Delete games directory

--- games/src/main/java/com/senac/games/repository/JogoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senac.games.repository;
-
-import com.senac.games.entity.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogoRepository extends JpaRepository<Jogo,Integer> {
-}
